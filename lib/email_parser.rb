f17fe8cee46e6d026631f792cf09df8270d886b0
Karen Flores <karen.ag.flores@gmail.com>
2018-06-27 02:23:18 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ def initialize(emails)
   @emails= emails
 end
 
-def self.parse
+def parse
   email_array = self.emails.split(" ") #separates emails by spaces
   email_array
   #how do we parse by multiple things....
